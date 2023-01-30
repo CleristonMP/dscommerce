209dd38c09fbeabe0e0722b3bab8878853aaca37
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-01-30 16:47:34 -0300

    User, Order and OrderStatus

---
+++ src/main/java/com/cmp/dscommerce/DscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.cmp.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cmp/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.cmp.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
