cf3931a8600e2dce5b052924edae8fc7824b5040
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-02-28 15:33:39 -0300

    Access control

--- src/main/java/com/cmp/dscommerce/controllers/ProductController.java
+++ src/main/java/com/cmp/dscommerce/controllers/ProductController.java
@@ -2,14 +2,15 @@ package com.cmp.dscommerce.controllers;
 
 import com.cmp.dscommerce.dtos.ProductDTO;
 import com.cmp.dscommerce.services.ProductService;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
@@ -33,6 +34,7 @@ public class ProductController {
         return ResponseEntity.ok(page);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
         dto = service.insert(dto);
@@ -40,12 +42,14 @@ public class ProductController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
